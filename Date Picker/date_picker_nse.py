132a83ea886694cd90f3ed20315dcdc88903749a
Abhishek Choudhary <ab.choudhary11@gmail.com>
2021-01-23 01:48:18 -0500

    file upload and date picker

---
+++ Date Picker/date_picker_nse.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www1.nseindia.com/products/content/derivatives/equities/archieve_fo.htm")
+driver.implicitly_wait(5)
+
+date_picker = driver.find_element_by_xpath("//input[@id='date']")
+date_picker.click()
+time.sleep(2)
+
+select_month = driver.find_element_by_xpath("//select[@class='ui-datepicker-month']")
+for option in select_month.find_elements_by_tag_name('option'):
+    if option.text == "May":
+        option.click()
+        break
+
+select_year = driver.find_element_by_xpath("//select[@class='ui-datepicker-year']")
+for option in select_year.find_elements_by_tag_name('option'):
+    if option.text == "2020":
+        option.click()
+        break
+
+days = driver.find_elements_by_xpath("//a[@class='ui-state-default']")
+for day in days:
+    if day.text == '10':
+        day.click()
+        break
+
+time.sleep(3)
+driver.quit()
---
+++ File Upload/file_upload.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://demo.guru99.com/test/upload/")
+driver.implicitly_wait(5)
+
+file_path = "D:\\Selenium with Python\\Selenium_Basics\\Web_Tables\\table_data.xlsx"
+
+driver.find_element_by_id("uploadfile_0").send_keys(file_path)
+time.sleep(1)
+driver.find_element_by_id("submitbutton").click()
+time.sleep(2)
+
+upload_success = driver.find_element_by_css_selector("h3[id='res'] center").is_displayed()
+if upload_success:
+    assert True, "File uploaded. Test Passed."
+else:
+    assert False, "File not uploaded. Test Failed"
+
+driver.quit()
